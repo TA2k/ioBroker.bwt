c77914b855d2c5ef6c25cb7c4b30b27ee4148a57
TA2k <tombox2020@gmail.com>
2021-11-21 21:53:11 +0100

    add content type

--- main.js
+++ main.js
@@ -72,7 +72,9 @@ class Bwt extends utils.Adapter {
             method: "post",
             url: "https://" + this.config.localIp + "/users/login?url=%2Fusers%2Flogin",
             headers: {
-                Accept: "*/*",
+                "Content-Type": "application/x-www-form-urlencoded",
+                Connection: "keep-alive",
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             },
             jar: this.cookieJar,
             withCredentials: true,
